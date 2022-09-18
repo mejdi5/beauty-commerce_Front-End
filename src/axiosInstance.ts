44f17be9604960eb1f7b76b10913e34afaae98e7
Mejdi Ben Ammou <mejdi.ben.ammou@gmail.com>
2022-09-18 16:07:23 +0200

    deploy to netlify

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = process.env.NODE_ENV === 'development' ? "http://localhost:5000" : "https://beauty-commerce.herokuapp.com"
+const baseUrl = process.env.NODE_ENV === 'development' ? "http://localhost:5000" : "https://beauty-commerce.adaptable.app"
 
 export const axiosInstance = axios.create({
     baseURL: baseUrl
